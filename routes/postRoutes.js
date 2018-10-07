932543fb41d6676e436433b85cb19460291abaa4
Zinzidane <ya-gans@mail.ru>
2018-10-07 12:42:13 +0300

    Verify jwt for protected routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const PostCtrl = require('../controllers/posts');
+const AuthHelper = require('../helpers/AuthHelper');
 
-router.post('/post/add-post', PostCtrl.AddPost);
+router.post('/post/add-post', AuthHelper.VerifyToken, PostCtrl.AddPost);
 
 module.exports = router;
