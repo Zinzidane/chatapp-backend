625ad69cd33baa849c2a0033931347caaf5f5637
Zinzidane <ya-gans@mail.ru>
2018-10-08 19:09:04 +0300

    Add like route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const PostCtrl = require('../controllers/posts');
 const AuthHelper = require('../helpers/AuthHelper');
 
-router.get('posts', AuthHelper.VerifyToken, PostCtrl.GetAllPosts);
+router.get('/posts', AuthHelper.VerifyToken, PostCtrl.GetAllPosts);
 router.post('/post/add-post', AuthHelper.VerifyToken, PostCtrl.AddPost);
+router.post('/post/add-like', AuthHelper.VerifyToken, PostCtrl.AddLike);
 
 module.exports = router;
--- socket/streams.js
+++ socket/streams.js
@@ -1,5 +1,8 @@
 module.exports = function(io) {
   io.on('connection', (socket) => {
-    
+    socket.on('refresh', (data) => {
+      console.log(data);
+      io.emit('refreshPage', {});
+    });
   });
 }
