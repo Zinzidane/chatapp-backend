53e6e079dab4f29fd0278e34e5cb31daf873d0c1
Zinzidane <ya-gans@mail.ru>
2018-10-10 12:55:19 +0300

    Add follow user controller

--- controllers/friends.js
+++ controllers/friends.js
@@ -3,10 +3,39 @@ const HttpStatus = require('http-status-codes');
 const User = require('../models/userModels');
 
 module.exports = {
-  async FollowUser(req, res) {
-    await User.find({})
-      .populate('posts.postId')
-      .then(users => res.status(HttpStatus.OK).json({message: 'All users', users}))
-      .catch(err => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'}));
+  FollowUser(req, res) {
+    const followUser = async () => {
+      await User.update({
+          _id: req.user._id,
+          // To prevent follow a user more than one time
+          "following.userFollowed": {$ne: req.body.userFollowed}
+        }, {
+          $push: {
+            following: {
+              userFollowed: req.body.userFollowed
+            }
+          }
+      });
+
+      await User.update({
+          _id: req.user.userFollowed,
+          // To prevent follow a user more than one time
+          "following.follower": {$ne: req.body._id}
+        }, {
+          $push: {
+            followers: {
+              follower: req.body._id
+            }
+          }
+      });
+    };
+
+    followUser()
+      .then(() => {
+        res.status(HttpStatus.OK).json({message: 'Following user now'})
+      })
+      .catch(err => {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'})
+      });
   }
 };
