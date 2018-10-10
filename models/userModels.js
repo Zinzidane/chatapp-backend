219c7af212c56a5665974e01ff17d6497ba3e19d
Zinzidane <ya-gans@mail.ru>
2018-10-10 18:16:19 +0300

    Update user model for notifications

--- models/userModels.js
+++ models/userModels.js
@@ -30,6 +30,32 @@ const userSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, ref: 'User'
       }
     }
+  ],
+  notifications: [
+    {
+      senderId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+      },
+      message: {
+        type: String
+      },
+      viewProfile: {
+        type: Boolean,
+        default: false
+      },
+      created: {
+        type: Date,
+        default: Date.now()
+      },
+      read: {
+        type: Boolean,
+        default: false
+      },
+      date: {
+        type: String,
+        default: ''
+      }
+    }
   ]
 });
 
