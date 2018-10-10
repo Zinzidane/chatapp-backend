d3a41c2fcf88b51cd8cf2f24f2e331ecdb27c2ca
Zinzidane <ya-gans@mail.ru>
2018-10-10 13:15:34 +0300

    Add methods for getting followers

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,25 @@ module.exports = {
   async GetAllUsers(req, res) {
     await User.find({})
       .populate('posts.postId')
-      .then(users => res.status(HttpStatus.OK).json({message: 'All users', users}))
+      .populate('following.userFollowed')
+      .populate('followers.follower')
+      .then(result => res.status(HttpStatus.OK).json({message: 'All users', result}))
+      .catch(err => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'}));
+  },
+  async GetUser(req, res) {
+    await User.findOne({_id: req.params.id})
+      .populate('posts.postId')
+      .populate('following.userFollowed')
+      .populate('followers.follower')
+      .then(result=> res.status(HttpStatus.OK).json({message: 'User by id', result}))
+      .catch(err => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'}));
+  },
+  async GetUserByName(req, res) {
+    await User.findOne({username: req.params.username})
+      .populate('posts.postId')
+      .populate('following.userFollowed')
+      .populate('followers.follower')
+      .then(result=> res.status(HttpStatus.OK).json({message: 'User by name', result}))
       .catch(err => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'}));
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,7 @@ const UserCtrl = require('../controllers/users');
 const AuthHelper = require('../helpers/AuthHelper');
 
 router.get('/users', AuthHelper.VerifyToken, UserCtrl.GetAllUsers);
+router.get('/user/:id', AuthHelper.VerifyToken, UserCtrl.GetUser);
+router.get('/user/username', AuthHelper.VerifyToken, UserCtrl.GetUserByName);
 
 module.exports = router;
