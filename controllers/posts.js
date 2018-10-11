b90885bbb4d6c5cc49aa287e4eb1517771779b32
Zinzidane <ya-gans@mail.ru>
2018-10-11 14:37:07 +0300

    Add top posts in get all posts controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -45,7 +45,12 @@ module.exports = {
         .populate('user')
         .sort({created: -1});
 
-      return res.status(HttpStatus.OK).json({message: 'All posts', posts});
+      // Get top posts
+      const top = await Post.find({totalLikes: {$gte: 2}})
+        .populate('user')
+        .sort({created: -1});
+
+      return res.status(HttpStatus.OK).json({message: 'All posts', posts, top});
     } catch (err) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'});
     }
