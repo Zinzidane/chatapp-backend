c53a2b8851a40315960254b6e9042aa2d6227ae0
Zinzidane <ya-gans@mail.ru>
2018-10-14 23:05:13 +0300

    Refactor server file

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const dbConfig = require('./config/secret');
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
+require('./socket/streams')(io);
+require('./socket/private')(io);
+
+const auth = require('./routes/authRoutes');
+const posts = require('./routes/postRoutes');
+const users = require('./routes/userRoutes');
+const friends = require('./routes/friendsRoutes');
+const message = require('./routes/messageRoutes');
+
 app.use(cors());
 
 app.use(express.json({limit: '50mb'}));
@@ -19,15 +28,6 @@ app.use(logger('dev'));
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.MONGO_URI, { useNewUrlParser: true });
-
-require('./socket/streams')(io);
-require('./socket/private')(io);
-
-const auth = require('./routes/authRoutes');
-const posts = require('./routes/postRoutes');
-const users = require('./routes/userRoutes');
-const friends = require('./routes/friendsRoutes');
-const message = require('./routes/messageRoutes');
 
 app.use('/api/chatapp', auth);
 app.use('/api/chatapp', posts);
