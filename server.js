65c820b780459ac32a04e3b7f6f57eb0492d8fbd
Zinzidane <ya-gans@mail.ru>
2018-10-14 23:03:20 +0300

    Edit server file

--- server.js
+++ server.js
@@ -11,23 +11,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET',
-    'POST',
-    'DELETE',
-    'PUT',
-    'OPTIONS'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
+
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use(cookieParser());
