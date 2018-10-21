189e832dad0819ed62e3623ae45c966868a36f76
Zinzidane <ya-gans@mail.ru>
2018-10-21 22:27:54 +0300

    Add and configure cloudinary

--- controllers/images.js
+++ controllers/images.js
@@ -1,5 +1,30 @@
+const cloudinary = require('cloudinary');
+const HttpStatus = require('http-status-codes');
+
+const User = require('../models/userModels');
+const keys = require('../config/secret');
+
+cloudinary.config({ 
+  cloud_name: keys.cloud_name, 
+  api_key: keys.api_key, 
+  api_secret: keys.api_secret
+});
+
 module.exports = {
   UploadImage(req, res) {
-    
+    cloudinary.v2.uploader.upload(req.body.image, async (error, result) => {
+      await User.update({
+        _id: req.user._id
+      }, {
+        $push: {
+          images: {
+            imgId: result.public_id,
+            imgVersion: result.version
+          }
+        }
+      })
+      .then(() => res.status(HttpStatus.OK).json({message: 'Image uploaded successfully'}))
+      .catch(err => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error uploading image'}));
+    });
   }
 };
