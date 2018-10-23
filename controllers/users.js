e464bda95b3a8048f8e585525ead427b581ec1cd
Zinzidane <ya-gans@mail.ru>
2018-10-23 20:29:47 +0300

    Update profile view method in user controller

--- controllers/users.js
+++ controllers/users.js
@@ -53,16 +53,23 @@ module.exports = {
     const dateValue = moment().format('YYYY-MM-DD');
     await User.update({
       _id: req.body.id,
-      'notifications.date': {$ne: dateValue}
+      'notifications.date': {$ne: [dateValue, '']},
+      'notifications.senderId': {$ne: req.user._id}
     }, {
       $push: {
         notifications: {
           senderId: req.user._id,
           message: `${req.user.username} viewed your profile`,
           created: new Date(),
-          date: dateValue
+          date: dateValue,
+          viewProfile: true
         }
       }
+    }).then(result=> {
+      res.status(HttpStatus.OK).json({message: 'Notification sent'});
+    })
+    .catch(err => {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'});
     });
   }
 };
